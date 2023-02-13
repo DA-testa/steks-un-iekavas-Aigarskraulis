bae0a5d8b2fb5669e81c40e8ef92fb53dd7165d6
Aigarskraulis <62945107+Aigarskraulis@users.noreply.github.com>
2023-02-13 09:18:19 +0000

    tt

--- main.py
+++ main.py
@@ -4,35 +4,34 @@ from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
-
 def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"] 
+    return (left + right) in ["()", "[]", "{}"]
+
 
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-             opening_brackets_stack.append(Bracket(next,i +1))
-            if next in ")]}":
-                
-            pass
-
+            opening_brackets_stack.append(Bracket(next, i + 1))
         if next in ")]}":
-            # Process closing bracket, write your code here
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1]/char,next):
-                     return i+1
-                    opening_brackets_stack.pop
-            pass
+             # Process closing bracket, write your code here
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i + 1
+            opening_brackets_stack.pop()
+
+    if not opening_brackets_stack:
+        return "Success"
+    else:
+        return opening_brackets_stack[-1].position
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    reult =  find_mismatch(text)
+     # Printing answer, write your code here
+    result = find_mismatch(text)
     print(result)
-
 
 
 if __name__ == "__main__":
