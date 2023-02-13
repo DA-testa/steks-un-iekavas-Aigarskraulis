e25f2593979da66b58d7600e7e1f0ee6e95031d1
Aigarskraulis <62945107+Aigarskraulis@users.noreply.github.com>
2023-02-13 09:11:12 +0000

    test

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next,i +1))
+            if next in ")]}":
+                
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1]/char,next):
+                    return i+1
+                    opening_brackets_stack.pop
             pass
 
 
@@ -25,6 +31,8 @@ def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    reult = find_mismatch(text)
+    print(result)
 
 
 if __name__ == "__main__":
